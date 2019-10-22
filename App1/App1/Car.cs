3a432b523586f6b5bbdcbd7436a17bba348c2f9f
Bartosz <bartuzi97@gmail.com>
2019-10-22 17:45:37 +0200

    Cos tam

---
+++ App1/App1/Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Car
+{
+    public string Marka;
+    public int Rok;
+
+    public string Model;
+    public int IloscDrzwi;
+    public double PojemnoscSilnika;
+    public double srednieSpalanie = 2.0;
+
+    public Car(string marka, int rok)
+    {
+        Marka = marka;
+        Rok = rok;
+    }
+
+    private double ObliczSpalanie(double dlugoscTrasy)
+    {
+        double spalanie;
+
+        spalanie = dlugoscTrasy * srednieSpalanie;
+
+        return spalanie;
+        
+    }
+
+    public double ObliczKosztPrzejazdu(double dlugoscTrasy, double cenaPaliwa)
+    {
+        double koszt;
+
+        koszt = ObliczSpalanie(dlugoscTrasy) * cenaPaliwa;
+
+        return koszt;
+    }
+
+}
---
+++ App1/App1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace App1
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("BMW", 1991);
+            Car car2 = new Car("Ferrari", 2010);
+
+            Console.WriteLine("Marka = {0} , Rok = {1}", car1.Marka, car1.Rok);
+            Console.WriteLine("Marka = {0} , Rok = {1}", car2.Marka, car2.Rok);
+
+           
+
+
+            Console.ReadKey();
+        }
+
+    }
+}

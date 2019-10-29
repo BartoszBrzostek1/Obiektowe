3ccb82443c04ea783f24ace906abe69db6809582
local <local@WMII-V7USKCH2QG>
2019-10-29 17:51:09 +0100

    Cw 3 ciag dalszy

---
+++ Wyjatki/Wyjatki/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wyjatki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int liczba1;
+            int liczba2 = 30;
+            int liczba3 = 0;
+
+            try
+            {
+                liczba1 = liczba2 / liczba3;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Finally!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

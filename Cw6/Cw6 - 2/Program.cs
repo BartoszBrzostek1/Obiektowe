be710c3af3add5743cfb01299c04987fb7876593
Bartosz <bartuzi97@gmail.com>
2019-11-12 17:16:08 +0100

    Cwiczenia 6

---
+++ Cw6/Cw6 - 2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cw6___2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Figura k = new Kwadrat();
+            Console.WriteLine("Pole wynosi: {0}",k.Pole(2,3,4));
+
+
+
+            Console.ReadLine();
+        }
+    }
+
+    abstract public class Figura
+    {
+        protected int a;
+        protected int b;
+        protected int c;
+
+
+        abstract public int Pole(int a, int b, int c);
+
+
+    }
+
+    public class Kwadrat : Figura
+    {
+
+        public override int Pole(int a, int b, int c)
+        {
+            int pole;
+            pole = a * b;
+            return pole;
+        }
+
+    }
+
+    public class Trojkat : Figura
+    {
+
+        public override int Pole(int a, int b, int c)
+        {
+            int pole;
+            pole = 1 / 2 * a * b;
+            return pole;
+        }
+    }
+}
---
+++ Cw6/Cw6/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cw6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Pracownik> pracowniks = new List<Pracownik>();
+            pracowniks.Add(new Programista());
+            pracowniks.Add(new Projektant());
+            pracowniks.Add(new Ksiegowy());
+
+            foreach(Pracownik p in pracowniks)
+            {
+                p.Pracuj();
+                
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Pracownik
+    {
+        public virtual void Pracuj()
+        {
+            Console.WriteLine("Pracownik pracuje");
+        }
+             
+    }
+
+    public class Programista : Pracownik
+    {
+        public override void Pracuj()
+        {
+            base.Pracuj();
+            Console.WriteLine("Obowiazki programisty");
+        }
+    }
+
+    public class Projektant : Pracownik
+    {
+        public override void Pracuj()
+        {
+            base.Pracuj();
+            Console.WriteLine("Obowiazki projektanta");
+        }
+    }
+
+    public class Ksiegowy : Pracownik
+    {
+        public override void Pracuj()
+        {
+            base.Pracuj();
+            Console.WriteLine("Obowiazki ksiegowego");
+        }
+
+    }
+        
+}

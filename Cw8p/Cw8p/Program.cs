df4507cdaf36a122dc796b1af025fe1785965be5
Bartosz <bartuzi97@gmail.com>
2019-11-26 17:48:46 +0100

    cw8 poprawione

---
+++ Cw8p/Cw8p/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Cw8p
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ArrayList myInt = new ArrayList();
+            myInt.Add(1);
+            myInt.Add(5);
+            ArrayList myStr = new ArrayList();
+            myStr.Add("Siwi");
+            myStr.Add("Wow!");
+            ArrayList myOso = new ArrayList();
+
+            Osoba o = new Osoba();
+            o.UstawImie("Bartosz");
+            o.UstawWiek(22);
+            myOso.Add(o);
+            
+           
+
+            Console.ReadLine();
+        }
+    }
+
+    class Osoba
+    {
+        private string imie;
+        private int wiek;
+
+        public Osoba()
+        {
+
+        }
+
+        public Osoba(string imie, int wiek)
+        {
+            this.imie = imie;
+            this.wiek = wiek;
+        }
+
+        public void UstawImie(string imie)
+        {
+            this.imie = imie;
+        }
+           
+
+        public void UstawWiek(int wiek)
+        {
+            this.wiek = wiek;
+        }
+    }
+
+}
+

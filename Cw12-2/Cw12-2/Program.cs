696e35902b7053ccad7ffcf6915d91646c0c95a3
Bartosz <bartuzi97@gmail.com>
2020-01-07 17:31:44 +0100

    Cw12

---
+++ Cw12-2/Cw12-2/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cw12_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kebab kebab1 = new GigaKebabCienki();
+            Console.WriteLine(kebab1.GetOpis() + " kosztuje: " + $"{kebab1.Koszt()}");
+
+            kebab1 = new Oliwki(kebab1);
+            Console.WriteLine(kebab1.GetOpis() + " kosztuje: " + $"{kebab1.Koszt()}");
+
+            kebab1 = new SosCzosnkowy(kebab1);
+            Console.WriteLine(kebab1.GetOpis() + " kosztuje: " + $"{kebab1.Koszt()}");
+            Console.ReadLine();
+
+     
+
+        }
+
+        public abstract class Kebab
+        {
+            public abstract double Koszt();
+            public abstract string GetOpis();
+            
+        }
+
+        class GigaKebabCienki : Kebab
+        {
+            double koszt = 5.00;
+
+
+
+            public override double Koszt()
+            {
+               
+                return koszt;
+            }
+
+            public override string GetOpis()
+            {
+                return "Giga Kebab na Cienkim";
+            }
+        }
+
+        public abstract class KebabDekorator : Kebab
+        {
+            protected Kebab kebab;
+            public KebabDekorator(Kebab _kebab)
+            {
+                kebab = _kebab;
+            }
+
+            public override string GetOpis()
+            {
+                return kebab.GetOpis();
+            }
+
+            public override double Koszt()
+            {
+                return kebab.Koszt();
+            }
+        }
+
+        class Oliwki : KebabDekorator
+        {
+            public Oliwki(Kebab _kebab):base(_kebab)
+            {
+
+            }
+            public override double Koszt()
+            {
+                return 0.50 + kebab.Koszt();
+            }
+            public override string GetOpis()
+            {
+                return kebab.GetOpis() + ", Oliwki";
+            }
+
+        }
+        class SerFeta : KebabDekorator
+        {
+            public SerFeta(Kebab _kebab) : base(_kebab)
+            {
+
+            }
+            public override double Koszt()
+            {
+                return 0.50 + kebab.Koszt();
+            }
+            public override string GetOpis()
+            {
+                return kebab.GetOpis() + ", Ser Feta";
+            }
+
+        }
+
+        class Cebula : KebabDekorator
+        {
+            public Cebula(Kebab _kebab) : base(_kebab)
+            {
+
+            }
+            public override double Koszt()
+            {
+                return 0.40 + kebab.Koszt();
+            }
+            public override string GetOpis()
+            {
+                return kebab.GetOpis() + ", Cebula";
+            }
+
+        }
+
+        class SosCzosnkowy : KebabDekorator
+        {
+            public SosCzosnkowy(Kebab _kebab) : base(_kebab)
+            {
+
+            }
+            public override double Koszt()
+            {
+                return 0.60 + kebab.Koszt();
+            }
+            public override string GetOpis()
+            {
+                return kebab.GetOpis() + ", Sos Czonskowy";
+            }
+
+        }
+
+        class SosMeksykanski : KebabDekorator
+        {
+            public SosMeksykanski(Kebab _kebab) : base(_kebab)
+            {
+
+            }
+            public override double Koszt()
+            {
+                return 0.60 + kebab.Koszt();
+            }
+            public override string GetOpis()
+            {
+                return kebab.GetOpis() + ", Sos Meksykanski";
+            }
+
+        }
+
+
+    }
+}
---
+++ Cw12/Cw12/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cw12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Napoj kawa1 = new MalaKawaZMlekiem();
+            Napoj czekolada1 = new SredniaGoracaCzekoladaBezMleka();
+            Console.WriteLine(kawa1.Opis + " kosztuje: " + $"{kawa1.Koszt}");
+            Console.WriteLine(czekolada1.Opis + " kosztuje: " + $"{czekolada1.Koszt}");
+            Console.ReadKey();
+
+        }
+
+        public abstract class Napoj
+        {
+            public string Opis { get { return this._opis; } } // get pobiera, set ustawia
+            public double Koszt { get { return this._koszt; } }
+            protected string _opis;
+            protected double _koszt;
+            protected bool mlekoBezLaktozy = false;
+            protected bool cukier = false;
+            protected bool maly = false;
+            protected bool sredni = false;
+            protected bool duzy = false;
+
+
+
+        }
+
+        class MalaKawaZMlekiem : Napoj
+        {
+            public MalaKawaZMlekiem()
+            {
+                this._opis = "Mala kawa z mlekiem";
+                this._koszt = 4.00;
+                
+                this.maly = true;
+                this.mlekoBezLaktozy = true;
+
+
+                if (this.mlekoBezLaktozy)
+                {
+                    this._koszt += 0.50;
+                }
+                if (this.cukier)
+                {
+                    this._koszt += 0.10;
+                }
+                if (this.maly)
+                {
+                    this._koszt += 1.00;
+                }
+            }
+        }
+
+        class SredniaGoracaCzekoladaBezMleka : Napoj
+        {
+            public SredniaGoracaCzekoladaBezMleka()
+            {
+                this._opis = "Srednia gorąca czegolada bez mleka";
+                this._koszt = 3.00;
+                
+                this.sredni = true;
+                this.cukier = true;
+
+                if (this.mlekoBezLaktozy)
+                {
+                    this._koszt += 0.50;
+                }
+                if (this.cukier)
+                {
+                    this._koszt += 0.10;
+                }
+                if (this.sredni)
+                {
+                    this._koszt += 2.00;
+                }
+
+            }
+
+
+        }
+
+
+    }
+}
